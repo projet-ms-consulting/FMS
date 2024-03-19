2b244e60be372d8c2a057640e160b2e77e8c6854
Fugann <95033152+Fugann@users.noreply.github.com>
2024-03-19 18:41:14 +0100

    create base admin dashboard

---
+++ src/Controller/Admin/Trait/AddDetails.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\Admin\Trait;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+
+trait AddDetails
+{
+    public function configureActions(Actions $actions): Actions
+    {
+        $actions = parent::configureActions($actions);
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL);
+    }
+}
