c7fed13d30580f08e095a37a86d53faabc3af2cb
Fugann <95033152+Fugann@users.noreply.github.com>
2024-03-25 11:07:01 +0100

    Update AppFixtures.php

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,12 +2,13 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Person;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class AppUserFixture extends Fixture
+class AppFixtures extends Fixture
 {
 
     public function __construct(private readonly UserPasswordHasherInterface $hasher)
@@ -16,13 +17,21 @@ class AppUserFixture extends Fixture
 
     public function load(ObjectManager $manager): void
     {
+        //Person
+        $person = new Person();
+        $person->setFirstname('John');
+        $person->setLastname('Doe');
+        $person->setCreatedAt(new \DateTimeImmutable());
+        $manager->persist($person);
+        $personAdmin[] = $person;
+
         $user = new User();
-        $user->setEmail('tomscherer29@example.com');
-        $user->setRoles(['ROLE_USER']);
-        $user->setPassword($this->hasher->hashPassword(
-            $user,
-            'password123'
-        ));
+        $user->setEmail('admin@admin.fr');
+        $user->setRoles(['ROLE_ADMIN']);
+        $hash = $this->hasher->hashPassword($user, 'admin');
+        $user->setPassword($hash);
+        $user->setPerson($personAdmin[0]);
+        $user->setCreatedAt(new \DateTimeImmutable());
         $manager->persist($user);
         $manager->flush();
     }
