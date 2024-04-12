03a6bdd85f0adf006ac34a4ac7d08895a14a0f82
Tom Scherer <106777338+Tomshr29@users.noreply.github.com>
2024-04-12 16:17:18 +0200

    Create bento grid home dashboard

--- src/Controller/Dashboard/DashboardController.php
+++ src/Controller/Dashboard/DashboardController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller\Dashboard;
 
+use App\Repository\CompanyRepository;
+use App\Repository\MissionRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,8 +13,16 @@ use Symfony\Component\Routing\Attribute\Route;
 class DashboardController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(UserRepository $userRepository, MissionRepository $missionRepository, CompanyRepository $companyRepository): Response
     {
-        return $this->render('dashboard/home/index.html.twig');
+        $userCount = $userRepository->count([]);
+        $missionCount = $missionRepository->count([]);
+        $companyCount = $companyRepository->count([]);
+
+        return $this->render('dashboard/home/index.html.twig', [
+            'userCount' => $userCount,
+            'missionCount' => $missionCount,
+            'companyCount' => $companyCount,
+        ]);
     }
 }
