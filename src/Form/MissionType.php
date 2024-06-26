2be9e6470c1ec56eb61b322a520855bb517187e3
Fugann <95033152+Fugann@users.noreply.github.com>
2024-06-26 13:53:43 +0200

    mission crud + suppression anglais

--- src/Form/MissionType.php
+++ src/Form/MissionType.php
@@ -15,9 +15,15 @@ class MissionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('price')
+            ->add('name', null, [
+                'label' => 'Nom'
+            ])
+            ->add('description', null, [
+                'label' => 'Description'
+            ])
+            ->add('price', null, [
+                'label' => 'Prix'
+            ])
             ->add('client', EntityType::class, [
                 'class' => company::class,
                 'choice_label' => 'name',
@@ -33,11 +39,20 @@ class MissionType extends AbstractType
                 'class' => company::class,
                 'choice_label' => 'name',
                 'placeholder' => 'Choisissez un manager',
-                'query_builder' => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('c')
-                        ->join('c.type', 't')
+                'query_builder' => function (EntityRepository $er) use ($options) {
+                    $currentManager = $options['data']->getManager();
+
+                    $qb = $er->createQueryBuilder('c');
+                    $qb->leftJoin('c.type', 't')
                         ->where('t.label = :type')
                         ->setParameter('type', 'Admin');
+
+                    if ($currentManager) {
+                        $qb->orWhere('c.id = :currentManagerId')
+                            ->setParameter('currentManagerId', $currentManager->getId());
+                    }
+
+                    return $qb;
                 },
             ])
         ;
