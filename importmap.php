279e1ed59acb2a73314417e3627d09feb91fc496
Fugann <95033152+Fugann@users.noreply.github.com>
2024-07-23 17:26:02 +0200

    composer update

--- importmap.php
+++ importmap.php
@@ -16,20 +16,20 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
-    '@hotwired/stimulus' => [
-        'version' => '3.2.2',
-    ],
     '@symfony/stimulus-bundle' => [
         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
     ],
+    '@symfony/ux-live-component' => [
+        'path' => './vendor/symfony/ux-live-component/assets/dist/live_controller.js',
+    ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
+    ],
     '@hotwired/turbo' => [
-        'version' => '7.3.0',
+        'version' => '8.0.5',
     ],
     'tom-select' => [
         'version' => '2.3.1',
-    ],
-    '@symfony/ux-live-component' => [
-        'path' => './vendor/symfony/ux-live-component/assets/dist/live_controller.js',
     ],
     'tom-select/dist/css/tom-select.bootstrap4.css' => [
         'version' => '2.3.1',
