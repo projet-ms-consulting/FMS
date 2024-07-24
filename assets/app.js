0ef34ed742b1430d06cbae418ef64ea32cfd5541
Fugann <95033152+Fugann@users.noreply.github.com>
2024-07-24 11:13:26 +0200

    sa fonctionne

--- assets/app.js
+++ assets/app.js
@@ -5,4 +5,3 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/css/app.css';
